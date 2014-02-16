49dcb91ad63e83f853c2fe772ab7947503ffad98
padisetty <siva@padisetty.com>
2014-02-15 16:37:32 -0800

    EC2 Launch initial scripts

---
+++ AWS/10 AD.ps1
@@ -0,0 +1,7 @@
+﻿Makecert -r -pe -n CN="sivaad.cloudapp.net" -eku 1.3.6.1.5.5.7.3.1 -ss my -sr localmachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12
+
+
+https://ec2-54-242-89-137.compute-1.amazonaws.com/FederationMetadata/2007-06/FederationMetadata.xml
+
+http://blogs.aws.amazon.com/security/post/Tx71TWXXJ3UI14/Enabling-Federation-to-AWS-using-Windows-Active-Directory-ADFS-and-SAML-2-0
+http://docs.aws.amazon.com/STS/latest/UsingSTS/STSMgmtConsole-SAML.html 
